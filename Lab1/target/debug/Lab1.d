a4978148d55699da0b65f27ba18ec47aa4c5c7ae
MCristi028 <manolachecristian06@gmail.com>
2025-10-06 19:29:56 +0300

    initial commit

---
+++ Lab1/target/debug/Lab1.d
@@ -0,0 +1,1 @@
+C:\RustProjects\Lab1\target\debug\Lab1.exe: C:\RustProjects\Lab1\src\main.rs
---
+++ Lab1/target/debug/deps/Lab1-ee89bad95de2cc59.d
@@ -0,0 +1,5 @@
+C:\RustProjects\Lab1\target\debug\deps\Lab1-ee89bad95de2cc59.d: src\main.rs
+
+C:\RustProjects\Lab1\target\debug\deps\libLab1-ee89bad95de2cc59.rmeta: src\main.rs
+
+src\main.rs:
