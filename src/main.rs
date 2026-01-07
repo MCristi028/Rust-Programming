05b2daf219df644dba7c630e89a93e3cde9265e4
MCristi028 <manolachecristian06@gmail.com>
2026-01-07 02:43:49 +0200

    added CLI commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(
+    name = "recursive_grep",
+    about = "Recursively search files in a directory for a substring or regex.",
+    version
+)]
+struct Args {
+    /// Search string
+    pattern: String,
+
+    /// Search directory
+    path: PathBuf,
+
+    /// Stop after printing # lines(default: inf)
+    #[arg(long = "max-lines")]
+    max_lines: Option<usize>,
+
+    /// Case-insensitive search
+    #[arg(long)]
+    ignore_case: bool,
+
+    /// Print only # of matches
+    #[arg(long)]
+    count: bool,
+
+    /// Search string as regex
+    #[arg(long)]
+    regex: bool,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+
+    // Test, print args
+    println!("{args:?}");
+
+    Ok(())
 }
