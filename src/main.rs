f11767e8e9429c0d36af54c7165d6119904b2d0d
MCristi028 <manolachecristian06@gmail.com>
2026-01-07 19:49:52 +0200

    directory traversal

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::path::PathBuf;
 
 use clap::Parser;
+use walkdir::WalkDir;
 
 #[derive(Parser, Debug)]
 #[command(
@@ -11,22 +12,17 @@ use clap::Parser;
 struct Args {
     /// Search string
     pattern: String,
-
     /// Search directory
     path: PathBuf,
-
-    /// Stop after printing # lines(default: inf)
+    /// Stop after printing # lines (default: infinite)
     #[arg(long = "max-lines")]
     max_lines: Option<usize>,
-
     /// Case-insensitive search
     #[arg(long)]
     ignore_case: bool,
-
     /// Print only # of matches
     #[arg(long)]
     count: bool,
-
     /// Search string as regex
     #[arg(long)]
     regex: bool,
@@ -35,8 +31,19 @@ struct Args {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
-    // Test, print args
-    println!("{args:?}");
+    for entry in WalkDir::new(&args.path) {
+        let entry = match entry {
+            Ok(e) => e,
+            Err(err) => {
+                eprintln!("Failed to read entry: {err}");
+                continue;
+            }
+        };
+
+        if entry.file_type().is_file() {
+            println!("{}", entry.path().display());
+        }
+    }
 
     Ok(())
 }
